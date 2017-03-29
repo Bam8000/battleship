fb5acb799356bfc204083d15894f4cafe0b23427
bam8000 <bam8000@gmail.com>
2017-03-28 20:22:29 -0400

    Change-Id: I1a153b0c230a1fa118bd38000bd177d53a1e993a

--- Battleship/src/battleship/Board.java
+++ Battleship/src/battleship/Board.java
@@ -1,5 +1,30 @@
 package battleship;
 
 public class Board {
+	private BoardType type;
+	private Cell[][] board;
+	private int[] shipCount;
+	
+	public Board() {
+		type = type.DISPLAYED;
+		board = new Cell[10][10];
+		shipCount = new int[6];
+		
+		shipCount[0] = 85; //The number of empty cells on the board (no ships)
+		for (int i=1; i<shipCount.length; i++) {
+			shipCount[i] = i; //each index > 0 corresponds to # of pieces of each ship.
+		}
+	}
+	
+	@Override
+	public String toString() {
+		//TODO
+	}
+	
+	//May not need to be public!!
+	//Needs to be robust.
+	public Cell getCellByCoord(String coord) {
+		
+	}
 
 }
