fe8f67234c843c5911d01e99bb2ac470970a82b4
bam8000 <bam8000@gmail.com>
2017-03-28 20:11:40 -0400

    Change-Id: Iee42babc82b2c09aef9ebace41d24b483fd856d8

--- Battleship/src/battleship/Cell.java
+++ Battleship/src/battleship/Cell.java
@@ -1,5 +1,47 @@
 package battleship;
 
 public class Cell {
-	//Hello World!Hiasa
+	private ShipState ship;
+	private CellState state;
+	
+	public Cell() {
+		ship = ShipState.ZERO;
+		state = CellState.EMPTY;
+	}
+	
+	/*
+	 * @param ShipState ship, an enum that determines what type of ship it is.
+	 * Sets the ship state of the cell to ShipState ship.
+	 */
+	public void setShip(ShipState ship) {
+		this.ship = ship;
+	}
+	
+	/*
+	 * @return ShipState ship.
+	 */
+	public ShipState getShip() {
+		return ship;
+	}
+	
+	/*
+	 * As if the cell is hit by a missile. Sets state to HIT or MISS depending on if there is a ship
+	 * and returns the corresponding integer to the value of enum ship.
+	 * @return int equivalent of enum value of ship.
+	 */
+	public int beHit() {
+		if (ship != ShipState.ZERO) state = CellState.HIT;
+		else state = CellState.MISS;
+		
+		switch (ship) {
+		case ZERO: return 0;
+		case ONE: return 1;
+		case TWO: return 2;
+		case THREE: return 3;
+		case FOUR: return 4;
+		case FIVE: return 5;
+		default: return -1; //Need to address this number, which could cause errors.
+		}
+		
+	}
 }
--- Battleship/src/battleship/ShipState.java
+++ Battleship/src/battleship/ShipState.java
@@ -2,4 +2,5 @@ package battleship;
 
 public enum ShipState {
 	FIVE, FOUR, THREE, TWO, ONE, ZERO
+	//ZERO means it has no ship.
 }
