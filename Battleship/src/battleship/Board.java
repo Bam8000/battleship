59c9e689df3ada09f55bdc13d7937e1ec3f3f219
bam8000 <bam8000@gmail.com>
2017-04-02 12:46:47 -0400

    Change-Id: Ie412cb52223eb7665cada54a67a5978fc88eca46

--- Battleship/src/battleship/Board.java
+++ Battleship/src/battleship/Board.java
@@ -1,5 +1,8 @@
 package battleship;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Board {
 	private BoardType type;
 	private Cell[][] board;
@@ -21,10 +24,49 @@ public class Board {
 		//TODO
 	}
 	
+	public void setType(BoardType type) {
+		this.type = type;
+	}
+	
 	//May not need to be public!!
 	//Needs to be robust.
 	public Cell getCellByCoord(String coord) {
 		
+		//make this a while loop
+		if (isValidCoord(coord)) {
+			String letter = coord.substring(0, 1).toUpperCase();
+			int number = Integer.parseInt(coord.substring(1)); //will it give a logic error?
+			int x;
+			int y = number;
+			
+			switch(letter) {
+			case "A": x = 0;
+			case "B": x = 1;
+			case "C": x = 2;
+			case "D": x = 3;
+			case "E": x = 4;
+			case "F": x = 5;
+			case "G": x = 6;
+			case "H": x = 7;
+			case "I": x = 8;
+			case "J": x = 9;
+			default: x = 0;
+			}
+			
+			return board[x][y];
+		}
+		return board[0][0]; //REMOVE!!
+	}
+	
+	public void hitCell(String coord) {
+		Cell cell = getCellByCoord(coord);
+		
+	}
+	
+	private boolean isValidCoord(String coord) {
+		Pattern p = Pattern.compile("[a-jA-J][1-10]");
+		Matcher m = p.matcher(coord);
+		return m.find();
 	}
 
 }
