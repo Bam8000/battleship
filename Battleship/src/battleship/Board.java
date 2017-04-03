2a021e0f337afcb4bf2d77337be42ad5d2b707c0
bam8000 <bam8000@gmail.com>
2017-04-03 13:37:01 -0400

    Change-Id: I7dd08c4df6ae455be0b093a214c6ac0042b86581

--- Battleship/src/battleship/Board.java
+++ Battleship/src/battleship/Board.java
@@ -8,8 +8,9 @@ public class Board {
 	private Cell[][] board;
 	private int[] shipCount;
 	
+	
 	public Board() {
-		type = type.DISPLAYED;
+		type = BoardType.DISPLAYED;
 		board = new Cell[10][10];
 		shipCount = new int[6];
 		
@@ -17,56 +18,184 @@ public class Board {
 		for (int i=1; i<shipCount.length; i++) {
 			shipCount[i] = i; //each index > 0 corresponds to # of pieces of each ship.
 		}
+		
+		for (int r=0; r<10; r++) {
+			for (int c=0; c<10; c++) {
+				board[r][c].setX(r);
+				board[r][c].setY(c);
+			}
+		}
 	}
+	
 	
 	@Override
 	public String toString() {
-		//TODO
+		String img = "    A  B  C  D  E  F  G  H  I  J \n";
+		if (type.equals(BoardType.DISPLAYED)) {
+			for (int r=0; r<10; r++) {
+				int num = r+1;
+				img += " " + num + " ";
+				for (int c=0; c<10; c++) {
+					if (board[r][c].getShip().equals(ShipState.ZERO)) img += " - ";
+					else img += " " + shipStateToInt(board[r][c].getShip()) + " ";
+				}
+				img += "\n";
+			}
+			return img;
+		} else {
+			for (int r=0; r<10; r++) {
+				int num = r+1;
+				img += " " + num + " ";
+				for (int c=0; c<10; c++) {
+					if (board[r][c].getState().equals(CellState.EMPTY)) img += " - ";
+					else if (board[r][c].getState().equals(CellState.MISS)) img += " o ";
+					else img += " X ";
+				}
+				img += "\n";
+			}
+			return img;
+		}
 	}
+	
 	
 	public void setType(BoardType type) {
 		this.type = type;
 	}
 	
+	
 	//May not need to be public!!
-	//Needs to be robust.
 	public Cell getCellByCoord(String coord) {
-		
-		//make this a while loop
-		if (isValidCoord(coord)) {
-			String letter = coord.substring(0, 1).toUpperCase();
-			int number = Integer.parseInt(coord.substring(1)); //will it give a logic error?
-			int x;
-			int y = number;
-			
-			switch(letter) {
-			case "A": x = 0;
-			case "B": x = 1;
-			case "C": x = 2;
-			case "D": x = 3;
-			case "E": x = 4;
-			case "F": x = 5;
-			case "G": x = 6;
-			case "H": x = 7;
-			case "I": x = 8;
-			case "J": x = 9;
-			default: x = 0;
-			}
-			
-			return board[x][y];
-		}
-		return board[0][0]; //REMOVE!!
+		return board[getXOfCoord(coord)][getYOfCoord(coord)];
 	}
+	
+	
+	//may not need to be public
+	public int getXOfCoord(String coord) {
+		String letter = coord.substring(0, 1).toUpperCase();
+		int x;
+		
+		switch(letter) {
+		case "A": x = 0;
+		case "B": x = 1;
+		case "C": x = 2;
+		case "D": x = 3;
+		case "E": x = 4;
+		case "F": x = 5;
+		case "G": x = 6;
+		case "H": x = 7;
+		case "I": x = 8;
+		case "J": x = 9;
+		default: x = 0;
+		}
+		
+		return x;
+	}
+	
+	
+	//may not need to be public
+	public int getYOfCoord(String coord) {
+		int number = Integer.parseInt(coord.substring(1)); //will it give a logic error?
+		return number;
+	}
+	
 	
 	public void hitCell(String coord) {
 		Cell cell = getCellByCoord(coord);
-		int i = cell.beHit();
+		
+		if (cell.getState().equals(CellState.EMPTY)) {
+			int i = cell.beHit();
+			shipCount[i] -= 1;
+			
+			if (shipCount[i] == 0) {
+				System.out.println("A ship has been sunk!");
+			}
+		} else {
+			System.out.println("You already shot at that cell!");
+		}
 	}
+	
+	
+	public boolean checkShip(ShipState type, String orientation, String coord) {
+		Cell cell = getCellByCoord(coord);
+		orientation = orientation.toLowerCase();
+		int ship = shipStateToInt(type);
+		
+		if (!isValidOrientation(orientation)) { //SHOULD THIS BE DONE HERE???
+			System.out.println("Invalid orientation");
+			return false;
+		}
+		
+		switch(orientation) {
+		case "up": 
+			for (int c=cell.getY(); c>(cell.getY()-ship); c--) {
+				if (c < 0 || board[cell.getX()][c].getShip() != ShipState.ZERO) {
+					return false;
+				}
+			} return true;
+		case "down":
+			for (int c=cell.getY(); c<(ship+cell.getY()); c++) {
+				if (c > 9 || board[cell.getX()][c].getShip() != ShipState.ZERO) {
+					return false;
+				}
+			} return true;
+		case "right":
+			for (int r=cell.getX(); r<(ship+cell.getX()); r++) {
+				if (r > 9 || board[r][cell.getY()].getShip() != ShipState.ZERO) {
+					return false;
+				}
+			} return true;
+		case "left":
+			for (int r=cell.getX(); r>(cell.getX()-ship); r--) {
+				if (r < 0 || board[r][cell.getY()].getShip() != ShipState.ZERO) {
+					return false;
+				}
+			} return true;
+		default: return true;
+		}
+	}
+	
+	
+	public void placeShip(ShipState type, String orientation, String coord) {
+		if (checkShip(type, orientation, coord)) {
+			 Cell cell = getCellByCoord(coord);
+			 int ship = shipStateToInt(type);
+			
+			switch(orientation) {
+			case "up": 
+				for (int c=cell.getY(); c>(cell.getY()-ship); c--) board[cell.getX()][c].setShip(type);
+			case "down":
+				for (int c=cell.getY(); c<(ship+cell.getY()); c++) board[cell.getX()][c].setShip(type);
+			case "right":
+				for (int r=cell.getX(); r<(ship+cell.getX()); r++) board[r][cell.getY()].setShip(type);
+			case "left":
+				for (int r=cell.getX(); r>(cell.getX()-ship); r--) board[r][cell.getY()].setShip(type);
+			}	
+		}
+	}
+	
 	
 	private boolean isValidCoord(String coord) {
 		Pattern p = Pattern.compile("[a-jA-J][1-10]");
 		Matcher m = p.matcher(coord);
 		return m.find();
+	}
+	
+	
+	private boolean isValidOrientation(String orientation) {
+		return orientation.equals("up") || orientation.equals("down") || 
+				orientation.equals("right") || orientation.equals("left");
+	}
+	
+	
+	private int shipStateToInt(ShipState ss) {
+		switch(ss) {
+		case ONE: return 1;
+		case TWO: return 2;
+		case THREE: return 3;
+		case FOUR: return 4;
+		case FIVE: return 5;
+		default: return 0; //possible logic error?
+		}
 	}
 
 }
--- Battleship/src/battleship/Cell.java
+++ Battleship/src/battleship/Cell.java
@@ -3,10 +3,14 @@ package battleship;
 public class Cell {
 	private ShipState ship;
 	private CellState state;
+	private int x;
+	private int y;
 	
 	public Cell() {
 		ship = ShipState.ZERO;
 		state = CellState.EMPTY;
+		x = 0;
+		y = 0;
 	}
 	
 	/*
@@ -17,6 +21,22 @@ public class Cell {
 		this.ship = ship;
 	}
 	
+	public void setX(int x) {
+		this.x = x;
+	}
+	
+	public void setY(int y) {
+		this.y = y;
+	}
+	
+	public int getX() {
+		return x;
+	}
+	
+	public int getY() {
+		return y;
+	}
+	
 	/*
 	 * @return ShipState ship.
 	 */
@@ -25,13 +45,25 @@ public class Cell {
 	}
 	
 	/*
+	 * @return CellState state
+	 */
+	public CellState getState() {
+		return state;
+	}
+	
+	/*
 	 * As if the cell is hit by a missile. Sets state to HIT or MISS depending on if there is a ship
 	 * and returns the corresponding integer to the value of enum ship.
 	 * @return int equivalent of enum value of ship.
 	 */
 	public int beHit() {
-		if (ship != ShipState.ZERO) state = CellState.HIT;
-		else state = CellState.MISS;
+		if (ship != ShipState.ZERO) {
+			state = CellState.HIT;
+			System.out.println("A ship has been hit!");
+		} else {
+			state = CellState.MISS;
+			System.out.println("Miss!");
+		}
 		
 		switch (ship) {
 		case ZERO: return 0;
