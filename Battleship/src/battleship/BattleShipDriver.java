283cde17f4236afcd69b8238a7d9571f4b82c949
Bam8000 <bam8000@gmail.com>
2017-04-13 19:16:29 -0400

    BattleShipDriver.java

--- Battleship/src/battleship/BattleShipDriver.java
+++ Battleship/src/battleship/BattleShipDriver.java
@@ -1,8 +1,7 @@
 package battleship;
 import java.util.*;
 public class BattleShipDriver {
-	
-	public static void main(String args[]) {
+	public static void main(String args[]){
 		String a;
 		String b;
 		Player player1=new Player();
@@ -16,13 +15,13 @@ public class BattleShipDriver {
 			a=input.nextLine();
 			System.out.println("Enter the coordinate in the from of x-axis first then y. For example A3 or D8");
 			b=input.nextLine();
-			player1.getBoard().placeShip(i,a,b){
+			player1.getBoard().placeShip(i,a,b);
 				
-			}
-			player1.getBoard().checkship(i,a,b){
-				
-			}
-			System.out.println(player1.getboard());
+			
+			player1.getBoard().checkShip(i,a,b);
+			
+			
+			System.out.println(player1.getBoard());
 
 	}
 		for(int c=0;c<20;c++){
@@ -38,27 +37,48 @@ public class BattleShipDriver {
 			a=input.nextLine();
 			System.out.println("Enter the coordinate in the from of x-axis first then y. For example A3 or D8");
 			b=input.nextLine();
-			player2.getboard.placeShip(i,a,b){
+			player2.getBoard().placeShip(i,a,b);
 				
-			}
-			player2.getboard.checkship(i,a,b){
+			
+			player2.getBoard().checkShip(i,a,b);
 				
-			}
-			System.out.println(player2.getboard());
+			
+			System.out.println(player2.getBoard());
 	}
 		for(int c=0;c<20;c++){
 			System.out.println("-----------------------------------------------------------------------------");
 		}
-		int e=0;
-		int f=0;
-		for (int d=1;d<6;d++){
-			e=e+player1.getboard.shipCount[d];
-			f=f+player2.getboard.shipCount[d];
-		}
+		
+		
+		int e=1;
+		int f=1;
 		while (e>0|| f>0){
 			for(int g=1;g<3;g++){
-				
+				if (g==1){
+					System.out.println("Player 1 please select a coordinate to fire");
+					System.out.println("Enter the coordinate in the from of x-axis first then y. For example A3 or D8");
+					String missile=input.nextLine();
+					player2.getBoard().hitCell(missile);
+				}
+				else{
+					System.out.println("Player 2 please select a coordinate to fire");
+					System.out.println("Enter the coordinate in the from of x-axis first then y. For example A3 or D8");
+					String missile=input.nextLine();
+					player1.getBoard().hitCell(missile);
+				}
+				e=0;
+				f=0;
+				for (int d=1;d<6;d++){
+					e=e+player1.getBoard().shipCount[d];
+					f=f+player2.getBoard().shipCount[d];
+			}
 			}
 		}
+		
 }
+	private boolean isValidOrientation(String orientation) {
+		return orientation.equals("up") || orientation.equals("down") || 
+				orientation.equals("right") || orientation.equals("left");
+	}
+	
 }
